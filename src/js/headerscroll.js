cc3923efd042449e49bcdf88b9c48f84974b2063
Salvador Hernández <zhavaman@gmail.com>
2017-06-14 19:35:36 -0500

    #1 indent headerscroll code

--- src/js/headerscroll.js
+++ src/js/headerscroll.js
@@ -2,24 +2,24 @@ var last_known_scroll_position = 0;
 var ticking = false;
 //navbar-fixed-top
 function doSomething(scroll_pos) {
-    if (window.location.pathname === '/project-rgv/') {
-        if (scroll_pos > 400) {
-            document.querySelector('.site-navbar').classList.add('site-navbar-blue');
-            document.querySelector('.site-navbar').classList.remove('site-navbar-transparent');
-        } else {
-            document.querySelector('.site-navbar').classList.add('site-navbar-transparent');
-            document.querySelector('.site-navbar').classList.remove('site-navbar-blue');
-        }
+  if (window.location.pathname === '/project-rgv/') {
+    if (scroll_pos > 400) {
+      document.querySelector('.site-navbar').classList.add('site-navbar-blue');
+      document.querySelector('.site-navbar').classList.remove('site-navbar-transparent');
+    } else {
+      document.querySelector('.site-navbar').classList.add('site-navbar-transparent');
+      document.querySelector('.site-navbar').classList.remove('site-navbar-blue');
     }
+  }
 }
 doSomething();
 window.addEventListener('scroll', function(e) {
-    last_known_scroll_position = window.scrollY;
-    if (!ticking) {
-        window.requestAnimationFrame(function() {
-            doSomething(last_known_scroll_position);
-            ticking = false
-        });
-    }
-    ticking = true;
+  last_known_scroll_position = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false
+    });
+  }
+  ticking = true;
 });

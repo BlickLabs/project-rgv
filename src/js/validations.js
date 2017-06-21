271308c613e1cb8a679151e7636b43e2d84a953b
Salvador Hernández <zhavaman@gmail.com>
2017-06-21 12:28:05 -0500

    #9 add contanct validations

--- src/js/validations.js
+++ src/js/validations.js
@@ -1,3 +1,43 @@
 (function () {
-  
+  $('#contact-form').validate({
+    rules: {
+      name: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      message: {
+        required: true,
+        minlength: 1,
+        maxlength: 140
+      }
+    }
+  });
+
+  $('#job-form').validate({
+    rules: {
+      name: {
+        required: true
+      },
+      birthday: {
+        required: true,
+        date: true,
+        minlength: 10
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      title: {
+        required: true
+      },
+      phone: {
+        required: true,
+        digits: true,
+        minlength: 8
+      }
+    }
+  });
 })();
